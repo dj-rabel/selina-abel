6e4b689bea97bc3f0ec7474ff20c1b06e5b972bf
Ruben Pascal Abel <ruben.p.abel@gmail.com>
2022-03-04 10:02:39 +0100

    Switch to ckeditor

--- assets/js/frontend-editing.js
+++ assets/js/frontend-editing.js
@@ -2,26 +2,76 @@
 
    const basePath = 'C:\\Users\\abel\\PhpstormProjects\\selina-abel\\storage\\';
 
+    class SimpleBoxEditing extends Plugin {
+        init() {
+            console.log( 'SimpleBoxEditing#init() got called' );
+
+            this._defineSchema();
+            this._defineConverters();                                              // ADDED
+        }
+
+        _defineSchema() {
+            // ...
+        }
+
+        _defineConverters() {                                                      // ADDED
+            const conversion = this.editor.conversion;
+
+            conversion.elementToElement( {
+                model: 'simpleBox',
+                view: {
+                    name: 'section',
+                    classes: 'simple-box'
+                }
+            } );
+
+            conversion.elementToElement( {
+                model: 'simpleBoxTitle',
+                view: {
+                    name: 'h1',
+                    classes: 'simple-box-title'
+                }
+            } );
+
+            conversion.elementToElement( {
+                model: 'simpleBoxDescription',
+                view: {
+                    name: 'div',
+                    classes: 'simple-box-description'
+                }
+            } );
+        }
+    }
+
+
    // -- EDU EDITOR
 
-   const eduelem = document.getElementById('education');
-   const educontent = eduelem.innerHTML;
-   eduelem.innerHTML = '';
+   const eduelem = document.getElementById('education').querySelector('.contenteditable');
 
-   // @see https://github.com/codex-team/editor.js/blob/next/types/configs/editor-config.d.ts
-   const editor = new EditorJS({
+   BalloonEditor
+       .create( eduelem)
+       .catch( error => {
+          console.error( error );
+       } );
 
-      holder: eduelem,
 
-      tools: {
-
-      },
-
-      onReady: () => {
-         editor.blocks.renderFromHTML(educontent);
-      }
-
-   });
+   // const educontent = eduelem.innerHTML;
+   // eduelem.innerHTML = '';
+   //
+   // // @see https://github.com/codex-team/editor.js/blob/next/types/configs/editor-config.d.ts
+   // const editor = new EditorJS({
+   //
+   //    holder: eduelem,
+   //
+   //    tools: {
+   //
+   //    },
+   //
+   //    onReady: () => {
+   //       editor.blocks.renderFromHTML(educontent);
+   //    }
+   //
+   // });
 
    // editor.save().then((outputData) => {
    //    console.log('Article data: ', outputData)
